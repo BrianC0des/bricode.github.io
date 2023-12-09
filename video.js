d04bfb63894e696b8acb2dc53e73c4fa7236292c
BrianC0des <115345092+BrianC0des@users.noreply.github.com>
2023-12-09 15:55:43 +0800

    Add files via upload

---
+++ video.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const videoContainer = document.getElementById("videoContainer");
+
+    // Replace these video IDs with your actual video IDs
+    const videoIds = ["womd8BFIbDY", "PlpM2LJWu-s", "YJTKlAvbDo4"];
+
+    // Loop through video IDs and create video iframes
+    videoIds.forEach(videoId => {
+        const iframe = document.createElement("iframe");
+        iframe.src = `https://www.youtube.com/embed/${videoId}`;
+        iframe.width = "560";
+        iframe.height = "315";
+        iframe.allowFullscreen = true;
+        videoContainer.appendChild(iframe);
+    });
+});
